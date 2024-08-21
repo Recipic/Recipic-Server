65032ab10a19bd53ca26c7c823e3eb3de642ffb5
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-21 14:27:20 +0900

    🚀project init
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

---
+++ src/main/java/CaffeineCoder/recipic/RecipicApplication.java
@@ -0,0 +1,13 @@
+package CaffeineCoder.recipic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CaffeineCoder/recipic/RecipicApplicationTests.java
@@ -0,0 +1,13 @@
+package CaffeineCoder.recipic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
