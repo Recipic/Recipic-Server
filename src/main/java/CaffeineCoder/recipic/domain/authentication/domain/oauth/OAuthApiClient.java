729290f5a157a3dced29c1a284f08bb31463227e
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-23 16:36:31 +0900

    ✨feat: Oauth 서버에 API 요청 후 응답값을 리턴받는 클래스

---
+++ src/main/java/CaffeineCoder/recipic/domain/authentication/domain/oauth/OAuthApiClient.java
@@ -0,0 +1,7 @@
+package CaffeineCoder.recipic.domain.authentication.domain.oauth;
+
+public interface OAuthApiClient {
+    OAuthProvider oAuthProvider();
+    String requestAccessToken(OAuthLoginParams params);
+    OAuthInfoResponse requestOauthInfo(String accessToken);
+}
