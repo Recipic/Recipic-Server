35db094bc1c98b448aa05584f7fc0cd658b0d99f
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-23 16:26:28 +0900

    ✨feat: add User domain 신규 API 목록 1. /api/user: accesstoken을 통해 userid를 받는 API 2. /api/users: 유저 목록 반환

---
+++ src/main/java/CaffeineCoder/recipic/domain/User/domain/User.java
@@ -0,0 +1,32 @@
+package CaffeineCoder.recipic.domain.User.domain;
+
+import CaffeineCoder.recipic.domain.authentication.domain.oauth.OAuthProvider;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    private String email;
+
+    private String nickname;
+
+    @Column(name = "o_auth_provider")
+    private OAuthProvider oAuthProvider;
+
+    @Builder
+    public User(String email, String nickname, OAuthProvider oAuthProvider) {
+        this.email = email;
+        this.nickname = nickname;
+        this.oAuthProvider = oAuthProvider;
+    }
+}
