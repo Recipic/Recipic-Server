b66d6eded768ceab1cb5d19514e1cacb3c4bed75
나은 <113881692+knxxxn@users.noreply.github.com>
2024-08-24 21:31:28 +0900

    Brand_id 데이터 형식 변환

--- src/main/java/CaffeineCoder/recipic/domain/brand/domain/Brand.java
+++ src/main/java/CaffeineCoder/recipic/domain/brand/domain/Brand.java
@@ -9,7 +9,7 @@ public class Brand {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "brand_id")
-    private Long brandId;
+    private Integer brandId;
 
     @Column(name = "brand_name")
     private String brandName;
--- src/main/java/CaffeineCoder/recipic/domain/brand/domain/BrandIngredientId.java
+++ src/main/java/CaffeineCoder/recipic/domain/brand/domain/BrandIngredientId.java
@@ -2,10 +2,15 @@ package CaffeineCoder.recipic.domain.brand.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
+@Getter
+@NoArgsConstructor
 public class BrandIngredientId implements Serializable {
 
     @Column(name = "brand_id")
@@ -14,4 +19,22 @@ public class BrandIngredientId implements Serializable {
     @Column(name = "ingredient_id")
     private Integer ingredientId;
 
+    public BrandIngredientId(Integer brandId, Integer ingredientId) {
+        this.brandId = brandId;
+        this.ingredientId = ingredientId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BrandIngredientId that = (BrandIngredientId) o;
+        return Objects.equals(brandId, that.brandId) &&
+                Objects.equals(ingredientId, that.ingredientId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(brandId, ingredientId);
+    }
 }
