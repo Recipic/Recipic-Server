4c462f022fa473770b99a62f1254dd8a16865b20
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-24 17:52:08 +0900

    ✨feat: JWT & security 추가에 따른 Oauth 로직 변경 JwtTokenProvider -> TokenProvider 사용

--- src/main/java/CaffeineCoder/recipic/domain/authentication/domain/AuthTokensGenerator.java
+++ src/main/java/CaffeineCoder/recipic/domain/authentication/domain/AuthTokensGenerator.java
@@ -1,6 +1,5 @@
 package CaffeineCoder.recipic.domain.authentication.domain;
 
-import CaffeineCoder.recipic.domain.authentication.infra.JwtTokenProvider;
 import CaffeineCoder.recipic.domain.jwtSecurity.controller.dto.TokenDto;
 import CaffeineCoder.recipic.domain.jwtSecurity.entity.RefreshToken;
 import CaffeineCoder.recipic.domain.jwtSecurity.jwt.TokenProvider;
@@ -14,23 +13,14 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor
 public class AuthTokensGenerator {
-    private static final String BEARER_TYPE = "Bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
-    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
 
-    private final JwtTokenProvider jwtTokenProvider;
-
     public TokenDto generate(Long memberId) {
-        long now = (new Date()).getTime();
-        Date accessTokenExpiredAt = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
-        Date refreshTokenExpiredAt = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
 
         String subject = memberId.toString();
         Collection<GrantedAuthority> authorities = new ArrayList<>();
