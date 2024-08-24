e6b30c5bcb4dc8e083fe4a43c448171053b8c91b
나은 <113881692+knxxxn@users.noreply.github.com>
2024-08-24 17:35:54 +0900

    Revert "레시피 게시글 등록 (#6)"
    This reverts commit 59a4a1b85d8da80be70ee12431e151ef692a6c33.

--- src/main/java/CaffeineCoder/recipic/recipe/dao/RecipeRepository.java
+++
@@ -1,9 +0,0 @@
-package CaffeineCoder.recipic.recipe.dao;
-
-import CaffeineCoder.recipic.recipe.domain.Recipe;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RecipeRepository extends JpaRepository<Recipe, Integer> {
-}
