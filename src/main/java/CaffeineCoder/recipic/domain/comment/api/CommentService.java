40ee583b0f1b1cc2021cc9cc567c8edf40d80c1b
나은 <113881692+knxxxn@users.noreply.github.com>
2024-08-25 22:42:36 +0900

    feat: 댓글 작성 수정

--- src/main/java/CaffeineCoder/recipic/domain/comment/api/CommentService.java
+++ src/main/java/CaffeineCoder/recipic/domain/comment/api/CommentService.java
@@ -6,6 +6,8 @@ import CaffeineCoder.recipic.domain.comment.dto.CommentRequestDto;
 import CaffeineCoder.recipic.domain.recipe.dao.RecipeRepository;
 import CaffeineCoder.recipic.domain.user.dao.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,8 +19,12 @@ public class CommentService {
     private final RecipeRepository recipeRepository;
 
     public boolean createComment(CommentRequestDto commentRequestDto) {
+        // JWT 토큰에서 userId 추출
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        Long userId = Long.parseLong(authentication.getName());
+
         // 유저와 레시피가 존재하는지 확인
-        if (!userRepository.existsById(commentRequestDto.getUserId())) {
+        if (!userRepository.existsById(userId)) {
             throw new IllegalArgumentException("Invalid userId");
         }
         if (!recipeRepository.existsById(commentRequestDto.getRecipeId())) {
@@ -27,7 +33,7 @@ public class CommentService {
 
         // 댓글 생성 및 저장
         Comment comment = Comment.builder()
-                .userId(commentRequestDto.getUserId())
+                .userId(userId)
                 .recipeId(commentRequestDto.getRecipeId())
                 .content(commentRequestDto.getComment())
                 .build();
