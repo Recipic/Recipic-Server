41fab4d1e7672299fc869d2d8e713bbfd8df3556
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-25 16:46:06 +0900

    ✨feat: refreshToken 쿠키로 반환
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/authentication/api/AuthController.java
+++ src/main/java/CaffeineCoder/recipic/domain/authentication/api/AuthController.java
@@ -3,7 +3,10 @@ package CaffeineCoder.recipic.domain.authentication.api;
 import CaffeineCoder.recipic.domain.authentication.infra.kakao.KakaoLoginParams;
 import CaffeineCoder.recipic.domain.authentication.infra.naver.NaverLoginParams;
 import CaffeineCoder.recipic.domain.jwtSecurity.controller.dto.TokenDto;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.antlr.v4.runtime.Token;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +17,32 @@ public class AuthController {
     private final OAuthLoginService oAuthLoginService;
 
     @PostMapping("/kakao")
-    public ResponseEntity<TokenDto> loginKakao(@RequestBody KakaoLoginParams params) {
-        return ResponseEntity.ok(oAuthLoginService.login(params));
+    public ResponseEntity<TokenDto> loginKakao(@RequestBody KakaoLoginParams params,HttpServletResponse response) {
+        TokenDto tokenDto = oAuthLoginService.login(params);
+        setRefreshTokenInCookie(response, tokenDto.getRefreshToken());
+        return ResponseEntity.ok(tokenDto);
     }
 
     @PostMapping("/naver")
-    public ResponseEntity<TokenDto> loginNaver(@RequestBody NaverLoginParams params) {
-        return ResponseEntity.ok(oAuthLoginService.login(params));
+    public ResponseEntity<TokenDto> loginNaver(@RequestBody NaverLoginParams params,HttpServletResponse response) {
+        TokenDto tokenDto = oAuthLoginService.login(params);
+        setRefreshTokenInCookie(response, tokenDto.getRefreshToken());
+        return ResponseEntity.ok(tokenDto);
     }
 
     @PostMapping("/admin")
-    public ResponseEntity<TokenDto> issueAdmin() {
-        return ResponseEntity.ok(oAuthLoginService.issueAdmin());
+    public ResponseEntity<TokenDto> issueAdmin(HttpServletResponse response) {
+        TokenDto tokenDto = oAuthLoginService.issueAdmin();
+        setRefreshTokenInCookie(response, tokenDto.getRefreshToken());
+        return ResponseEntity.ok(tokenDto);
+    }
+
+    private void setRefreshTokenInCookie(HttpServletResponse response, String refreshToken) {
+        Cookie cookie = new Cookie("refreshToken", refreshToken);
+        cookie.setHttpOnly(true);  // JS로 쿠키 접근 못하게 설정 (보안 강화)
+        cookie.setPath("/");  // 쿠키가 유효한 경로 설정
+        cookie.setMaxAge(7 * 24 * 60 * 60);  // 쿠키의 만료 시간 설정 (예: 7일)
+        response.addCookie(cookie);
     }
 
 
