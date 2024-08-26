c893ef3bb927f84a1889d99a6a83160a196d24a1
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-26 14:44:21 +0900

    ✨feat: 코멘트 수가 들어간 반환용 Dto구현
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/comment/dao/CommentRepository.java
+++ src/main/java/CaffeineCoder/recipic/domain/comment/dao/CommentRepository.java
@@ -2,8 +2,12 @@ package CaffeineCoder.recipic.domain.comment.dao;
 
 import CaffeineCoder.recipic.domain.comment.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+    @Query("SELECT COUNT(*) FROM Comment WHERE recipeId = :recipeId")
+    Integer countByRecipeId(@Param("recipeId") Integer recipeId);
 }
