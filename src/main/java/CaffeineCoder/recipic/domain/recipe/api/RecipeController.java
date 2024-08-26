85f0467ac8f0609b2f308930a4cfd0c5d4abc8f9
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-26 14:29:59 +0900

    Feat/recipe/list: 전체 레시피 반환 & 쿼리 기능 구현 (#45)
    * ✨feat: 브랜드 이름에 따른 아이디 반환 쿼리 구현
    
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>
    
    * ✨feat: 전체 레시피 리스트 반환 & 쿼리 기능 구현
    
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>
    
    * ✨feat: 스크랩 수가 들어간 반환용 Dto구현
    
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>
    
    ---------
    
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/recipe/api/RecipeController.java
+++ src/main/java/CaffeineCoder/recipic/domain/recipe/api/RecipeController.java
@@ -1,10 +1,11 @@
 package CaffeineCoder.recipic.domain.recipe.api;
 
 import CaffeineCoder.recipic.domain.recipe.application.RecipeService;
+import CaffeineCoder.recipic.domain.recipe.dto.RecipeResponseDto;
 import CaffeineCoder.recipic.domain.recipe.dto.RecipeRequestDto;
 import CaffeineCoder.recipic.global.response.ApiResponse;
 import CaffeineCoder.recipic.global.util.ApiUtils;
-import lombok.Getter;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,6 +31,15 @@ public class RecipeController {
     @GetMapping("/detail/{recipeId}")
     public ApiResponse<?> getRecipeDetail(@PathVariable("recipeId") Integer recipeId) {
         return ApiUtils.success(recipeService.getRecipeDetail(recipeId));
+    }
+
+    @GetMapping("/list")
+    public ApiResponse<?> getScraps(
+            @RequestParam(value = "keyword", defaultValue = "") String keyword,
+            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "size", defaultValue = "10") int size) {
+        System.out.printf("keyword: %s, page: %d, size: %d\n", keyword, page, size);
+        return ApiUtils.success(recipeService.getQueriedRecipes(keyword, page, size));
     }
 
 }
