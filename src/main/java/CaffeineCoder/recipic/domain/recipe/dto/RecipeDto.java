292542babb058be8ac57fdb60196524bb9c95637
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-26 14:23:48 +0900

    ✨feat: 스크랩 수가 들어간 반환용 Dto구현
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/recipe/dto/RecipeDto.java
+++ src/main/java/CaffeineCoder/recipic/domain/recipe/dto/RecipeDto.java
@@ -1,36 +1,22 @@
 package CaffeineCoder.recipic.domain.recipe.dto;
 
-import CaffeineCoder.recipic.domain.recipe.domain.Recipe;
+import java.sql.Timestamp;
 
-public record RecipeDto (
-    String recipeId,
-    String userId,
-    String brandId,
+public record RecipeDto(
+    Integer recipeId,
+    Long userId,
+    Integer brandId,
     String title,
     String description,
     String imageUrl,
-    String isCelebrity,
-    String createdAt,
-    String status,
-    int scrapCount
+    Boolean isCelebrity,
+    Timestamp createdAt,
+    Integer status
 ) {
-    public static RecipeDto of(String recipeId, String userId, String brandId, String title, String description, String imageUrl, String isCelebrity, String createdAt, String status,int scrapCount) {
-        return new RecipeDto(recipeId, userId, brandId, title, description, imageUrl, isCelebrity, createdAt, status, scrapCount);
-    }
 
-    //추후 댓글 개수도 추가
-    public static RecipeDto fromEntity(Recipe recipe, int scrapCount) {
-        return new RecipeDto(
-                recipe.getRecipeId().toString(),
-                recipe.getUserId().toString(),
-                recipe.getBrandId().toString(),
-                recipe.getTitle(),
-                recipe.getDescription(),
-                recipe.getImageUrl(),
-                recipe.getIsCelebrity().toString(),
-                recipe.getCreatedAt().toString(),
-                recipe.getStatus().toString(),
-                scrapCount
-        );
+    public static RecipeDto of(Integer recipeId, Long userId, Integer brandId, String title, String description, String imageUrl, Boolean isCelebrity, Timestamp createdAt, Integer status) {
+        return new RecipeDto(recipeId, userId, brandId, title, description, imageUrl, isCelebrity, createdAt, status);
     }
 }
+
+
