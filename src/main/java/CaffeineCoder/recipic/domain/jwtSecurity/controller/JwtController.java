c8c959f6550ff22ea4a4ef1660ed44635d666038
KangManJoo <eogns47@konkuk.ac.kr>
2024-08-27 14:33:53 +0900

    🛠️fix: API 명세 변경
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/controller/JwtController.java
+++ src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/controller/JwtController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/jwt")
 @RequiredArgsConstructor
 public class JwtController {
     private final AuthService authService;
