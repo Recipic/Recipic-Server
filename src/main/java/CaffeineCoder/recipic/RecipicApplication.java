aaf3bdbf9339273c395a25e66130e53a9775583b
KangManJoo <eogns47@konkuk.ac.kr>
2024-09-19 23:20:34 +0900

    🚨hotfix: bean 스캔 안되는 문제 해결
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/RecipicApplication.java
+++ src/main/java/CaffeineCoder/recipic/RecipicApplication.java
@@ -3,9 +3,11 @@ package CaffeineCoder.recipic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@ComponentScan(basePackages = "CaffeineCoder.recipic.global.config")
 public class RecipicApplication {
 
 	public static void main(String[] args) {
