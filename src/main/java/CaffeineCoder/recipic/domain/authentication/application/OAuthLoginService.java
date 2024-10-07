46c367fcd0d01404f8f5390cfd77900b5ec1362d
KangManJoo <eogns47@konkuk.ac.kr>
2024-10-08 02:53:14 +0900

    feat: 로그아웃,회원 탈퇴 기능 구현
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/authentication/application/OAuthLoginService.java
+++ src/main/java/CaffeineCoder/recipic/domain/authentication/application/OAuthLoginService.java
@@ -1,14 +1,14 @@
-package CaffeineCoder.recipic.domain.authentication.api;
+package CaffeineCoder.recipic.domain.authentication.application;
 
 import CaffeineCoder.recipic.domain.authentication.domain.AuthTokensGenerator;
 import CaffeineCoder.recipic.domain.authentication.domain.oauth.OAuthInfoResponse;
 import CaffeineCoder.recipic.domain.authentication.domain.oauth.OAuthLoginParams;
 import CaffeineCoder.recipic.domain.authentication.domain.oauth.RequestOAuthInfoService;
-import CaffeineCoder.recipic.domain.authentication.infra.kakao.KakaoInfoResponse;
 import CaffeineCoder.recipic.domain.authentication.infra.kakao.admin.AdminInfoResponse;
 import CaffeineCoder.recipic.domain.user.domain.User;
 import CaffeineCoder.recipic.domain.user.dao.UserRepository;
 import CaffeineCoder.recipic.domain.jwtSecurity.controller.dto.TokenDto;
+import CaffeineCoder.recipic.global.util.RedisUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -18,10 +18,15 @@ public class OAuthLoginService {
     private final UserRepository userRepository;
     private final AuthTokensGenerator authTokensGenerator;
     private final RequestOAuthInfoService requestOAuthInfoService;
+    private final RedisUtil redisUtil;
+    private final long ACCESS_TOKEN_EXPIRATION = 3600000;
+
 
     public TokenDto login(OAuthLoginParams params) {
         OAuthInfoResponse oAuthInfoResponse = requestOAuthInfoService.request(params);
         Long memberId = findOrCreateMember(oAuthInfoResponse);
+
+        saveUserInfoToRedis(oAuthInfoResponse);
 
         return authTokensGenerator.generate(memberId);
     }
@@ -41,6 +46,16 @@ public class OAuthLoginService {
                 .build();
 
         return userRepository.save(user).getUserId();
+    }
+
+    private void saveUserInfoToRedis(OAuthInfoResponse oAuthInfoResponse) {
+        System.out.printf("OAuthInfoResponse: %s\n", oAuthInfoResponse);
+        System.out.println("Redis에 저장합니다.");
+        System.out.printf("key: %s, value: %s\n", "USER: " + oAuthInfoResponse.getEmail(), oAuthInfoResponse.getAccessToken());
+        String key = "USER: " + oAuthInfoResponse.getEmail(); // Redis에 저장할 키
+        String value = oAuthInfoResponse.getAccessToken(); // 저장할 값 (닉네임을 예시로 사용)
+
+        redisUtil.setValuesWithTimeout(key, value, ACCESS_TOKEN_EXPIRATION); // 1시간 동안 유효
     }
 
     public TokenDto issueAdmin() {
--- src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/service/JwtService.java
+++ src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/service/JwtService.java
@@ -6,12 +6,24 @@ import CaffeineCoder.recipic.domain.jwtSecurity.controller.dto.TokenRequestDto;
 import CaffeineCoder.recipic.domain.jwtSecurity.entity.RefreshToken;
 import CaffeineCoder.recipic.domain.jwtSecurity.jwt.TokenProvider;
 import CaffeineCoder.recipic.domain.jwtSecurity.repository.RefreshTokenRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.JwtParserBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+import static CaffeineCoder.recipic.domain.jwtSecurity.jwt.JwtTokenExpiration.ACCESS_TOKEN_EXPIRE_TIME;
+import static CaffeineCoder.recipic.domain.jwtSecurity.jwt.JwtTokenExpiration.REFRESH_TOKEN_EXPIRE_TIME;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +33,6 @@ public class JwtService {
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
-
 
 
     @Transactional
@@ -39,7 +50,7 @@ public class JwtService {
                 .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
 
         // 4. Refresh Token 일치하는지 검사
-        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+        if (!refreshToken.getToken().equals(tokenRequestDto.getRefreshToken())) {
             throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
         }
 
@@ -47,10 +58,18 @@ public class JwtService {
         TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
 
         // 6. 저장소 정보 업데이트
-        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
-        refreshTokenRepository.save(newRefreshToken);
+        refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(refreshToken, REFRESH_TOKEN_EXPIRE_TIME);
+
 
         // 토큰 발급
         return tokenDto;
     }
+
+
+    public long extractAccessExpiration() {
+        return ACCESS_TOKEN_EXPIRE_TIME;
+    }
+
+
 }
