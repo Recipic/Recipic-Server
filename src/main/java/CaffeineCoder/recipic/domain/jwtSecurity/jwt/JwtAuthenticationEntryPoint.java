6c3ad998741e744052cb8b3ed7d7643f335b8ccf
KangManJoo <eogns47@konkuk.ac.kr>
2024-10-22 21:36:13 +0900

    feat: 인증 오류 처리를 시큐리티 내 처리에서 글로벌 예외 처리로 위임
    Signed-off-by: KangManJoo <eogns47@konkuk.ac.kr>

--- src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/jwt/JwtAuthenticationEntryPoint.java
+++ src/main/java/CaffeineCoder/recipic/domain/jwtSecurity/jwt/JwtAuthenticationEntryPoint.java
@@ -1,19 +1,32 @@
 package CaffeineCoder.recipic.domain.jwtSecurity.jwt;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 
 @Component
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
+    private final HandlerExceptionResolver resolver;
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    public JwtAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
+        this.resolver = resolver;
+    }
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        // 유효한 자격증명을 제공하지 않고 접근하려 할때 401
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        String accept = request.getHeader("Accept");
+
+
+            resolver.resolveException(request, response, null, authException);
+
     }
 }
